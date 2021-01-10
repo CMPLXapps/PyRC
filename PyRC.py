56523f98f166d83411b02e83a442455c16deae30
cmplxapps <cmplxapps@gmail.com>
2021-01-10 00:13:50 -0800

    Current Progress 1/9/2021

---
+++ PyRC.py
@@ -0,0 +1,3 @@
+import os
+os.system('title PyRC')
+import main
---
+++ main.py
@@ -0,0 +1,9 @@
+import threading as thr
+import socket as sock
+import pytools as pt
+import threadscripts.threfs as thref
+import resources as rs
+import subprocess as sp
+import os
+import sys
+#----------------------------------------------------
