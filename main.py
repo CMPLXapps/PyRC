5c49f836146f9af7c4ac0c631ca5c47635cd13b6
cmplxapps <cmplxapps@gmail.com>
2021-01-12 14:29:39 -0800

    Current Progress 1/12/2021

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import threadscripts as thrs
 import threading as thr
 import socket as sock
 import pytools as pt
-import threadscripts.threfs as thref
 import resources as rs
 import subprocess as sp
 import os
 import sys
+import json as js
 #----------------------------------------------------
--- resources/__init__.py
+++ resources/__init__.py
@@ -1,2 +1,16 @@
 __all__ = []
 #----------------------------------------------------
+class DEF_NETCLASS:
+	def ROOM_conf(local_port, partner_port, partner_ipv4,  **kwargs):
+		global NETCLASS
+		class NETCLASS:
+			class port:
+				def __init__(self):
+					self.local = local_port
+					self.remote = partner_port
+			class ipv4:
+				def __init__(self):
+					self.local = sock.gethostname()
+					self.remote = partner_ipv4
+			class ipv6:
+				pass
