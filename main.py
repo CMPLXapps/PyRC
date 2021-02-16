b18e219df8cf0d3c21cb3751a95a48ccd18068ae
cmplxapps <cmplxapps@gmail.com>
2021-02-16 00:03:42 -0800

    Current Progress 2.16.2021

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threadscripts as thrs
-import threading as thr
-import socket as sock
-import pytools as pt
-import resources as rs
-import subprocess as sp
-import os
-import sys
-import json as js
+#!usr/bin/env python3
+import threadscripts as thrs, threading as thr, socket as sock, modules.pytools as pt, modules.keyboard as kb, shutil as sh
+import blessed as crs, resources as rs, subprocess as sp, os, sys, json as js
+#import modules.pyclass as pc
 #----------------------------------------------------
---
+++ modules/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['pytools', 'keyboard', 'pyclass', 'blessed']
