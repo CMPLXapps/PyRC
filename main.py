f8b9da56431880fd7df051f2164a8b2b7f30761d
CMPLX <jamesgrevie@gmail.com>
2021-05-28 18:00:21 -0700

    Current Progress 5.28.2021

--- main.py
+++
@@ -1,5 +0,0 @@
-#!usr/bin/env python3
-import threadscripts as thrs, threading as thr, socket as sock, modules.pytools as pt, modules.keyboard as kb, shutil as sh
-import blessed as crs, resources as rs, subprocess as sp, os, sys, json as js
-#import modules.pyclass as pc
-#----------------------------------------------------
--- modules/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['pytools', 'keyboard', 'pyclass', 'blessed']
